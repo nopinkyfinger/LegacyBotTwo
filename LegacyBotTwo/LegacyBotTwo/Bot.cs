703f502acb5dbfff1c15b50b3df0c2b952fb519b
Joey <nopinkeyfinger@gmail.com>
2018-05-03 22:11:13 -0500

    Moved most of the code out of Program.cs and into Bot.cs

---
+++ LegacyBotTwo/LegacyBotTwo/Bot.cs
@@ -0,0 +1,51 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+// Eventually I'll move most of the functions over here, since having it all
+// in main just seems silly
+
+namespace LegacyBotTwo
+{
+    public class Bot
+    {
+        private DiscordSocketClient client;
+
+        public Bot()
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task MainAsync()
+        {
+            client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info
+            });
+            string path = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            client.Log += Log;
+            client.MessageReceived += MessageReceived;
+
+            string token = System.IO.File.ReadAllText(path + "/token.txt");
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+                await message.Channel.SendMessageAsync("Pong!");
+        }
+    }
+}
+
--- LegacyBotTwo/LegacyBotTwo/Program.cs
+++ LegacyBotTwo/LegacyBotTwo/Program.cs
@@ -1,41 +1,14 @@
-﻿using Discord;
-using Discord.WebSocket;
-using System;
+﻿using System;
 using System.Threading.Tasks;
-using System.Reflection;
 
 namespace LegacyBotTwo
 {
     class Program
     {
-        public static void Main(string[] args)
-            => new Program().MainAsync().GetAwaiter().GetResult();
-
-        public async Task MainAsync()
+        public static int Main()
         {
-            var client = new DiscordSocketClient();
-            string path = System.AppDomain.CurrentDomain.BaseDirectory;
-
-            client.Log += Log;
-            client.MessageReceived += MessageReceived;
-
-            string token = System.IO.File.ReadAllText(path + "/token.txt");
-            await client.LoginAsync(TokenType.Bot, token);
-            await client.StartAsync();
-
-            await Task.Delay(-1);
-        }
-
-        private Task Log(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
-        }
-
-        private async Task MessageReceived(SocketMessage message)
-        {
-            if (message.Content == "!ping")
-                await message.Channel.SendMessageAsync("Pong!");
+            new Bot();
+            return 0;
         }
     }
 }
