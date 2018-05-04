74d5b77353280543f2c8ae144aead4fe83afea6f
Joey <nopinkeyfinger@gmail.com>
2018-05-03 21:20:08 -0500

    Set up the framework so it can successfully connect to Discord.

--- LegacyBotTwo/LegacyBotTwo/Program.cs
+++ LegacyBotTwo/LegacyBotTwo/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+using System.Reflection;
 
 namespace LegacyBotTwo
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            var client = new DiscordSocketClient();
+            string path = System.AppDomain.CurrentDomain.BaseDirectory;
+
+            client.Log += Log;
+            client.MessageReceived += MessageReceived;
+
+            string token = System.IO.File.ReadAllText(path + "/token.txt");
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+                await message.Channel.SendMessageAsync("Pong!");
         }
     }
 }
